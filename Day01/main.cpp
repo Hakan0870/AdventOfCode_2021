cf6b9d10bb1b14a7e8f32a490bfa16777ae89408
Hakan Ucar <hakan0870@gmail.com>
2021-12-02 00:49:30 +0100

    task two added

--- Day01/main.cpp
+++ Day01/main.cpp
@@ -26,9 +26,37 @@ int task_one() {
   return count;
 }
 
+int task_two() {
+  std::ifstream fin;
+  fin.open("input.txt");
+
+  std::vector<int> data;
+  int num;
+  while (fin >> num) {
+    data.push_back(num);
+  }
+  fin.close();
+
+  int current_total;
+  int count = 0;
+  int prev_total = data[0] + data[1] + data[2];
+
+  for (int i = 1; i < data.size() - 2; i++) {
+    current_total = 0;
+    for (int j = 0; j < 3; j++) {
+      current_total += data[i + j];
+    }
+
+    if (current_total > prev_total) {
+      count++;
+    }
+    prev_total = current_total;
+  }
+  return count;
+}
+
 
 int main() {
   cout << "result of task one: " << task_one() << endl;
-
-
+  cout << "result of task two: " << task_two() << endl;
 }
